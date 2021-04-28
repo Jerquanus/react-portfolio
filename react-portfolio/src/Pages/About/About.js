da7d132b9c4ec1bb77f189b3b732e607612e7572
Jerquanus Swann <jerquanus@Jerquanuss-MacBook-Air.local>
2021-04-27 23:20:52 -0400

    picture changes

--- react-portfolio/src/Pages/About/About.js
+++ react-portfolio/src/Pages/About/About.js
@@ -6,43 +6,35 @@ import swann from "../../Assets/images/sunswann.jpg";
 function About (){
     return(
         <div className="container">   
-           <div className="grid">
-               
+           <div className="grid"> 
                 <div> 
                     <img className="pic" src = {swann} alt="Jerquanus"  />
                 </div>
             
-                <div style={{color:'white', height: '300px', width: '100%'}} className="talking">
+                <div style={{color:'white'}} className="talking">
 
                     <p>
-                        If you're here - well that obviously means you wanted to know a little more about me.
-                        The name is Swann - Jerquanus Swann but I usually respond to Jerry or Swann. I'm a pisces
-                        sun and a tarus moon, I love long walks on th... wait wrong site.
+                        Hey, I'm Jerquanus (aka Jerry) and if you're here - well that obviously means you wanted to know a little more about who I am.
+                        I'm a pisces sun and a tarus moon, I love long walks on th... wait wrong site. But seriously, I come from a small country 
+                        town in North Carolina called Johnsonville. It's locacted in between Sanford and Fayetteville.
                     </p>
-                    
+        
                     <p>
-                        But seriously, I come from a small country town in North Carolina called Johnsonville. It's
-                        locacted in between Sanford
-                        and Fayetteville. I have well over a decade of retail/grocery store experience with 
+                        I have well over a decade of retail/grocery store experience with 
                         a little management experience sprinkled in there as well. 
 
-                        I've recently completed the University of North Carolina at Charlotte's 6 month intensive "Coding Bootcamp". 
+                        I've recently completed the University of North Carolina at Charlotte's 6 month intensive "Coding Bootcamp" - where I concentrated 
+                        on becoming a fullstack web developer. 
                         This is a huge industry change for me but I'm not afraid to work my way from the bottom to get to where I want to be.
-                        I'm a creative person and I love watching my visions come to life.
+                        I'm a creative person and I love watching my visions come to life. 
                     </p>
+
                     <p>
-                        I'm currently engaged to my beautiful fiance Niara and we're set to get married this September.
-                        My biggest hobbies in my down time are music production and watching anime. 
-                    
+                        I am also currently engaged to my gorgeous fiance Niara - we're set to get married this September.
                     </p>
-
+                    
                 </div>
-
-
-
            </div>
-
-
         </div>
     )
     
